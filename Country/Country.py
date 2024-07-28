2199dfa54d1089cd879d582c16c6e90436a8193f
Kylestew35 <87539893+Kylestew35@users.noreply.github.com>
2024-07-28 12:23:09 -0700

    Update Country.py

---
+++ Country/Country.py
@@ -0,0 +1,12 @@
+Write a program to perform the following functionality.
+Part 1
+Call a function that will display a heading and menu choices for the user.
+Part 2
+Call a function that will prepopulate a dictionary that will hold country key and name. A minimum of three countries must be in the original dictionary.
+Part 3
+Use the menu commands to perform the following functionality:
+Call a function to view a country. Display all the keys in the dictionary using a for..in loop. The user will then enter a key and display the corresponding country. If the user enters an invalid key display a message. Use the dictionary as a parameter passed into the function.
+Call a function to add a country to the dictionary. If the user enters a key value that already exists display a message. Use the dictionary as a parameter passed into the function.
+Call a function to delete a title from the dictionary. If the user enters an invalid key value display a message. Use the dictionary as a parameter passed into the function.
+Exit the program.
+If the user entered an invalid command display a message.
